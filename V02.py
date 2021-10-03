c778490a0c7fee2ecc92efbf29c2c10272e64515
MuhammadAnas789 <manas2019@namal.edu.pk>
2021-10-03 21:20:03 +0500

    Version02 By Anas

---
+++ V02.py
@@ -0,0 +1,21 @@
+Calculator V02.py
+
+#add function
+
+def add(num1, num2):
+	return num1 + num2
+
+num1 = 45
+num2 = 13
+
+if name == "__main__":
+	print(add(num1, num2))
+
+
+
+def div (num1,num2):
+       return num1/num2
+
+#main
+
+print(div(15,3))
