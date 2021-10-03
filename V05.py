7e8e00b50c5f6b8de521513affa3ac6475e378a3
MIAN ZUBAIR <zubair6986597@gmail.com>
2021-10-03 12:14:37 +0500

    Version 5 by Mian Zubair

---
+++ V05.py
@@ -0,0 +1,15 @@
+#add function
+# Mul function by Mian Zubair
+
+def add(num1, num2):
+	return num1 + num2
+
+def mul(num1,num2):
+	return num1 * num2
+
+num1 = 45
+num2 = 13
+
+if name == "__main__":
+	print(add(num1, num2))
+	print(mul(num1,num2))
