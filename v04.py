3d96280bb7c1a7fce5227e4641de695b3e3aaf9b
namal <jawad2019@namal.edu.pk>
2021-10-03 12:09:12 +0500

    version 4 by jawad

---
+++ v04.py
@@ -0,0 +1,23 @@
+# calculator in  Python(v0.4) by Jawad
+
+# Square Root Function
+def SqRoot(num):
+	return num ** (1/2)
+
+# Main
+if __name__ == "__main__":
+	print(SqRoot(25))
+
+
+
+
+#add function
+
+def add(num1, num2):
+	return num1 + num2
+
+num1 = 45
+num2 = 13
+
+if name == "__main__":
+	print(add(num1, num2))
