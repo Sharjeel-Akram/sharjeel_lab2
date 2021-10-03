585815c2aabfbf8bcd5a33d38778f345c31a6fa0
danish khan <danish2019@namal.edu.pk>
2021-10-03 07:38:44 +0500

    Third commit

---
+++ V03.py
@@ -0,0 +1,14 @@
+#add function (V01) by Sharjeel
+def add(num1, num2):
+	return num1 + num2
+
+#Sub function(V03) by Danish
+def sub(num1,num2):
+	return num1 - num2
+
+num1 = 45
+num2 = 13
+
+if name == "__main__":
+	print(add(num1, num2))
+	print(sub(num1,num2))
